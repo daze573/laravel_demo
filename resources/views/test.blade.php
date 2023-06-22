db8f49a3dc4ac07d70813dfc1cd2c1e9a34495bc
EC2 Default User <ec2-user@ip-172-31-35-148.ap-northeast-1.compute.internal>
2023-06-22 13:34:57 +0000

    form作成

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,3 +2,10 @@
     echo "Hello World";
 ?>
 <a href="/">home</a>
+<h1>フォームの作成テスト</h1>
+<form method="POST" action="form/php">
+  <label name="name">名前</label><br>
+  <input type="text" name="name"><br>
+  <label name="body">自己紹介</label><br>
+  <textarea type="text" name="body"></textarea>
+</form>
